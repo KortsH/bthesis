3ed7eb058ab1b4e739f8e749c8249db76e21d948
KortsH <122207777+KortsH@users.noreply.github.com>
2025-05-22 19:41:26 +0200

    commented out the temp post to server

--- hash_on_blockchain/server.js
+++ hash_on_blockchain/server.js
@@ -16,7 +16,7 @@ const chain = new Blockchain(2, "./chain.json");
 app.get("/quotes", (req, res) => {
   let sql = `
     SELECT id, platform, poster, post_id, content, post_time, tweet_url
-    FROM quotes
+      FROM quotes
   `;
   const params = [];
   if (req.query.search) {
@@ -27,47 +27,48 @@ app.get("/quotes", (req, res) => {
     sql += params.length ? " AND poster=?" : " WHERE poster=?";
     params.push(req.query.author);
   }
+
   db.all(sql, params, (err, rows) => {
     if (err) return res.status(500).json({ error: err.message });
     res.json(rows);
   });
 });
 
-app.post("/quotes", (req, res) => {
-  const { platform, poster, post_id, content, post_time, tweet_url } = req.body;
-  const stmt = db.prepare(`
-    INSERT INTO quotes(platform, poster, post_id, content, post_time, tweet_url)
-    VALUES (?, ?, ?, ?, ?, ?)
-  `);
-  stmt.run(
-    platform,
-    poster,
-    post_id,
-    content,
-    post_time,
-    tweet_url,
-    function (err) {
-      if (err) return res.status(500).json({ error: err.message });
-      const recordId = this.lastID;
+// app.post("/quotes", (req, res) => {
+//   const { platform, poster, post_id, content, post_time, tweet_url } = req.body;
+//   const stmt = db.prepare(`
+//     INSERT INTO quotes(platform, poster, post_id, content, post_time, tweet_url)
+//     VALUES (?, ?, ?, ?, ?, ?)
+//   `);
+//   stmt.run(
+//     platform,
+//     poster,
+//     post_id,
+//     content,
+//     post_time,
+//     tweet_url,
+//     function (err) {
+//       if (err) return res.status(500).json({ error: err.message });
+//       const recordId = this.lastID;
 
-      const fullData = {
-        platform,
-        poster,
-        post_id,
-        content,
-        post_time,
-        tweet_url,
-      };
-      const commitment = crypto
-        .createHash("sha256")
-        .update(JSON.stringify(fullData))
-        .digest("hex");
+//       const fullData = {
+//         platform,
+//         poster,
+//         post_id,
+//         content,
+//         post_time,
+//         tweet_url,
+//       };
+//       const commitment = crypto
+//         .createHash("sha256")
+//         .update(JSON.stringify(fullData))
+//         .digest("hex");
 
-      const newBlock = chain.addBlock({ recordId, commitment });
-      res.json({ recordId, commitment, block: newBlock });
-    }
-  );
-});
+//       const newBlock = chain.addBlock({ recordId, commitment });
+//       res.json({ recordId, commitment, block: newBlock });
+//     }
+//   );
+// });
 
 app.get("/chain", (req, res) => {
   res.json(chain.chain);
